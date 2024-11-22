62a5f5aa5eda6543d756d8cc2e3a968e73c251db
Vinicius <vimepes@outlook.com>
2024-11-22 13:38:58 -0300

    portifoliodash

---
+++ main.py
@@ -0,0 +1,101 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+from datetime import timedelta
+
+#carregar os dados
+
+st.cache_data
+def carregar_dados(acao):
+    texto_tickers = " ".join(acao)
+    dados_acao = yf.Tickers(texto_tickers)
+    cotacoes_acao = dados_acao.history(period ="1d", start = "2020-01-01", end = "2023-12-30")
+    cotacoes_acao = cotacoes_acao["Close"]
+    return cotacoes_acao
+st.cache_data
+def carregar_acoes():
+    base_acoes = pd.read_csv("IBOV.csv", sep=";")
+    tickers = list(base_acoes['Código'])
+    tickers = [item + ".SA" for item in tickers]
+    return tickers
+acoes =["ITUB4.SA", "PETR4.SA", "MGLU3.SA", "VALE3.SA", "ABEV3.SA", "GGBR4.SA"]
+
+#preparação
+acoes = carregar_acoes()
+dados = carregar_dados(acoes)
+lista_acoes = st.sidebar.multiselect("Escolha as ações a visualizar", dados.columns)
+if lista_acoes:
+    dados = dados[lista_acoes]
+    if len(lista_acoes) == 1:
+        unica_acao = lista_acoes[0]
+        dados = dados.rename(columns={unica_acao:"Close"})
+
+data_min = dados.index.min().to_pydatetime()
+data_max = dados.index.max().to_pydatetime()
+intervalo_data = st.sidebar.slider("Selecione o periodo desejado", min_value=data_min, max_value=data_max, value=(data_min, data_max),step=timedelta(days=1))
+dados = dados.loc[intervalo_data[0]:intervalo_data[1]]
+
+    
+#intertface
+st.title("""
+# Cotação das ações
+""")
+
+data_min = dados.index.min().to_pydatetime
+data_max = dados.index.max().to_pydatetime
+
+
+st.sidebar.header("Configurações") 
+
+st.line_chart(dados)
+
+
+
+texto_performance_ativos = ""   
+
+if len(lista_acoes) == 0:
+    lista_acoes = list(dados.columns)
+elif len(lista_acoes) == 1:
+    dados = dados.rename(columns={"Close": unica_acao})
+
+
+
+
+
+carteira = [1000 for acao in lista_acoes]
+total_inicial_carteira = sum(carteira)
+
+for i, acao in enumerate(lista_acoes):
+    performance_ativo = dados[acao].iloc[-1] / dados[acao].iloc[0] - 1
+    performance_ativo = float(performance_ativo)
+
+    carteira[i] = carteira[i] * (1 + performance_ativo)
+
+    if performance_ativo > 0:
+        # :cor[texto]
+        texto_performance_ativos = texto_performance_ativos + f"  \n{acao}: :green[{performance_ativo:.1%}]"
+    elif performance_ativo < 0:
+        texto_performance_ativos = texto_performance_ativos + f"  \n{acao}: :red[{performance_ativo:.1%}]"
+    else:
+        texto_performance_ativos = texto_performance_ativos + f"  \n{acao}: {performance_ativo:.1%}"
+
+total_final_carteira = sum(carteira)
+performance_carteira = total_final_carteira / total_inicial_carteira - 1
+
+if performance_carteira > 0:
+    texto_performance_carteira = f"Performance da carteira com todos os ativos: :green[{performance_carteira:.1%}]"
+elif performance_carteira < 0:
+    texto_performance_carteira = f"Performance da carteira com todos os ativos: :red[{performance_carteira:.1%}]"
+else:
+    texto_performance_carteira = f"Performance da carteira com todos os ativos: {performance_carteira:.1%}"
+
+
+
+st.write(f"""
+### Performance dos Ativos
+Essa foi a perfomance de cada ativo no período selecionado:
+
+{texto_performance_ativos}
+
+{texto_performance_carteira}
+""")
